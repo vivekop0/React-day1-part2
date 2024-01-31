cce8a7d75c7520a52118334fa56072a217e8f31f
vivekop0 <sarkarjii534@gamil.com>
2024-01-31 14:17:28 +0530

    adeed

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,49 @@
+/* eslint-disable react/jsx-key */
+/* eslint-disable react/prop-types */
 import { useState } from "react";
-
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
+
+const [todos,setTodo] =useState([{
+    title:"my name is Vivek",
+    description:"this is my Description"
+  }])
+  function addTodo(){
+
+    setTodo([...todos,{
+      title:"vivek",
+      description:"Sarkar"
+    }])
+  }
 
   return (
     <>
-      <h1>Counter App {count}</h1>
-      <div className="card">
-        <button onClick={() => setCount(count + 1)}>count is</button>
+    <input type="text" value={"hello"}></input>
+      {/* <CoustomButton count={count} setCount={setCount}></CoustomButton> */}
+      <button onClick={addTodo}> Add a todos</button>
+      {todos.map((todo)=>{
+          return <Todo title={todo.title} description={todo.description}></Todo>
 
-        <button onClick={() => setCount((count) => count - 1)}>Remove</button>
-      </div>
+      })}
     </>
   );
+}
+
+// function CoustomButton(props) {
+//   function onClickHandler() {
+//     props.setCount(props.count + 1);
+//   }
+
+//   return <button onClick={onClickHandler}>Counter {props.count}</button>;
+// }
+
+function Todo(prop){
+  return <>
+    <h1>{prop.title}</h1>
+    <h1>{prop.description}</h1>
+  </>
 }
 
 export default App;
